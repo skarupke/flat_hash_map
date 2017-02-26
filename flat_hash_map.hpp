0e58360c9c13aef26b9910c29a97a755385547a4
Malte Skarupke <malteskarupke@web.de>
2017-02-26 14:37:39 -0500

    Adding license

--- flat_hash_map.hpp
+++ flat_hash_map.hpp
@@ -1,3 +1,7 @@
+//          Copyright Malte Skarupke 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <cstdint>
